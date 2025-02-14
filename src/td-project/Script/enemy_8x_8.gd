d0e6320e4c86ad3880d43d85ebe4e6bd215f28ce
Nicolas Garric <nicolasdfgarric@gmail.com>
2025-02-14 14:40:25 +0100

    Adding groups && Modifications to enemy script

--- src/td-project/Script/enemy_8x_8.gd
+++ src/td-project/Script/enemy_8x_8.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+# Enemy 8x8 params
+
+var health = 5
+var speed = 30
+
+var 
--- src/td-project/Script/game.gd
+++ src/td-project/Script/game.gd
@@ -3,5 +3,29 @@ extends Node2D
 var money = 25
 var wave = 0
 var mobs_left = 0
-var wave_mobs = [5, 10, 25, 50, 80, 120]
+var wave_mobs = [5, 10, 25, 50, 80, 0]
 var wave_speed = [1, 1, 1, 1, 1]
+
+var instance
+var enemy = preload("res://Art/enemy8x8.png")
+
+func _ready() -> void:
+	%WaveTimer.start()
+
+
+func _on_wave_timer_timeout() -> void:
+	mobs_left = wave_mobs[wave]
+	%EnemyTimer.wait_time = wave_speed[wave]
+	%EnemyTimer.start()
+
+func _on_enemy_timer_timeout() -> void:
+	instance = enemy.instance()
+	$Path2D.add_child(instance)
+	mobs_left -= 1
+	if mobs_left <= 0:
+		%EnemyTimer.stop()
+		wave += 1
+		if wave < len(wave_mobs):
+			%WaveTimer.start()
+		else:
+			get_tree().change_scene_to_file("res://Scene/win.tscn")
